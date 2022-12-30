e1e04529f0f2692104e27e79614d253a0bf22258
AbelPalominoRojas <abelpr87@gmail.com>
2022-12-30 09:55:04 -0500

    Editorial Api FindAll

--- src/views/editorial/index.tsx
+++ src/views/editorial/index.tsx
@@ -1,4 +1,24 @@
+import { useEffect } from 'react';
+import { EditorialService } from '../../services';
+import { EditorialModel } from '../../types';
+
 const index = (): JSX.Element => {
+	// Attributes
+
+	// Hooks
+	useEffect(() => {
+		console.log('useEffect');
+		void findAllEditoriales();
+	}, []);
+
+	// Methods
+	const findAllEditoriales = async (): Promise<void> => {
+		const response = await EditorialService.findAll();
+		const data: EditorialModel[] = response.data;
+
+		console.log('Editoriales', data);
+	};
+
 	return <div>Editorial</div>;
 };
 
